ba5dae2258abdd169f485a91392cfc6871ef4d61
Harry Newman <96374072+veronin1@users.noreply.github.com>
2025-03-28 14:26:17 +0000

    Add files via upload
    button added

--- gui.py
+++ gui.py
@@ -1,6 +1,13 @@
 from tkinter import *
 import tkinter as tk
 
+"""
+7 Inputs - userName, age, height, weight, steps, heartRate, waterIntake
+7 Labels - userName, age, height, weight, steps, heartRate, waterIntake
+1 Button
+"""
+
+
 # Create window
 master = Tk()
 
@@ -8,8 +15,11 @@ master = Tk()
 pane = Frame(master)
 pane.pack(fill = BOTH, expand = True)
 
-b1 = Button(pane, text = "Button")
+b1 = Button(pane, text = "Button", background = "green", fg = "gray30")
 b1.pack(fill = BOTH, expand = True)
+
+b1 = Button(pane, text = "Button", background = "PeachPuff3", fg = "gray30")
+b1.pack(fill = BOTH, expand = True, padx=100)
 
 master.mainloop()
 
