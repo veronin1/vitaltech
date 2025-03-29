d9e8ecd3d6fa98e9416461d11ddfd5bf66456b9b
Harry Newman <h.newman2005@outlook.com>
2025-03-29 17:25:31 +0000

    Added labels/entry boxes

--- vitaltech_app_incl_gui/gui.py
+++ vitaltech_app_incl_gui/gui.py
@@ -1,25 +1,46 @@
 from tkinter import *
 import tkinter as tk
 
-"""
-7 Inputs - userName, age, height, weight, steps, heartRate, waterIntake
-7 Labels - userName, age, height, weight, steps, heartRate, waterIntake
-1 Button
-"""
+class GUI:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry("450x600")
+        self.root.title("Vitaltech Fitness App")
 
+        self.widgets()
+        self.root.mainloop()  # Moved here after everything is built
 
-# Create window
-master = Tk()
+    def widgets(self):
+        # --------- App Title ---------
+        self.title_label = tk.Label(self.root, text="Vitaltech Fitness App", font=('Aptos', 18, "bold"))
+        self.title_label.pack(pady=20)
 
-# Create an expandable frame
-pane = Frame(master)
-pane.pack(fill = BOTH, expand = True)
+        # --------- User Info ---------
+        self.user_info_label = tk.Label(self.root, text="User Info:", font=('Aptos', 14))
+        self.user_info_label.pack(anchor="w", padx=10)
 
-b1 = Button(pane, text = "Button", background = "green", fg = "gray30")
-b1.pack(fill = BOTH, expand = True)
+        self.name_label = tk.Label(self.root, text="Name: ")
+        self.name_label.pack(side=LEFT, padx= 10)
+        self.name_entry = tk.Entry(self.root)
+        self.name_entry.pack(side=LEFT)
 
-b1 = Button(pane, text = "Button", background = "PeachPuff3", fg = "gray30")
-b1.pack(fill = BOTH, expand = True, padx=100)
+        self.name_label = tk.Label(self.root, text="Age: ")
+        self.name_label.pack(anchor="w", padx= 10)
+        self.name_entry = tk.Entry(self.root)
+        self.name_entry.pack(anchor="w", padx= 10)
 
-master.mainloop()
+        self.name_label = tk.Label(self.root, text="Height: ")
+        self.name_label.pack(anchor="e", padx= 10)
+        self.name_entry = tk.Entry(self.root)
+        self.name_entry.pack(anchor="e", padx= 10)
 
+        self.name_label = tk.Label(self.root, text="Weight: ")
+        self.name_label.pack(anchor="e", padx= 10)
+        self.name_entry = tk.Entry(self.root)
+        self.name_entry.pack(anchor="e", padx= 10)
+
+def main():
+    app = GUI()
+
+if __name__ == "__main__":
+    main()
